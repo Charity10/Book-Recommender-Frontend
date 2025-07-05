6c58c322b8cb5d60d6ba5832881c944c9b9e1255
Daniel Adeneye <adeneyedaniel007@gmail.com>
2025-07-05 10:54:36 +0100

    update: setup react, vite, typescript and tailwindcss with folder structures

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,19 @@
+function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-4">
+      <h1 className="text-6xl text-blue-950 text-center">
+        Book Recommender Application
+      </h1>
+      <h4 className="text-4xl text-blue-900 text-center">
+        Welcome to the Book Recommender App
+      </h4>
+      <p className="text-2xl text-gray-700 text-center max-w-2xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, nesciunt
+        tempore numquam neque, ut nulla, alias fugit natus officiis excepturi
+        ipsa reiciendis illum magnam aliquid placeat ratione tenetur ea fugiat.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
